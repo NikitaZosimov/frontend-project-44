2a5a121554b38ea20086fc6cbbb68162f9b4eb31
Nikita Zosimov <nikitazos.of@gmail.com>
2022-09-12 11:35:25 +0300

    update

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -7,14 +7,14 @@ const brainProgression = () => {
       const firstProgressionNumber = getRandomNumbers(1, 100);
       const differenceBetweenTheNumbers = getRandomNumbers(1, 10);
       const indexOfHiddenNumber = getRandomNumbers(0, 9);
-      let progressionNum = [firstProgressionNumber];         
+      let progressionNum = [firstProgressionNumber];
       for (let i = 0; i < 9; i += 1) {
          progressionNum[i + 1] = progressionNum[i] + differenceBetweenTheNumbers;
       };
       const correctAnswer = String(progressionNum[indexOfHiddenNumber]);
       progressionNum[indexOfHiddenNumber] = '..';
       const question = progressionNum.join(' ');
-      countRounds.push([question, correctAnswer]);   
+      countRounds.push([question, correctAnswer]);
    };
    const task = 'What number is missing in the progression?';
    const game = [countRounds, task].flat();
