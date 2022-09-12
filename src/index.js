6e031caef799762ca6081e2244bcbdddd88b6e70
Nikita Zosimov <nikitazos.of@gmail.com>
2022-09-12 12:09:56 +0300

    update

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const gameRules = (game) => {
          console.log('Correct!');
          i += 1;
       } else {
-         console.log(`Your answer: '${userAnswer}' is wrong answer;(. Correct answer was ${correctAnswer})`);
+         console.log(`'${userAnswer}' is wrong answer;(. Correct answer was ${correctAnswer})`);
          return `Let's try again, ${userName}!`;
       };
    };
