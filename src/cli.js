882498ec0cd6c1a02287021f594cfa14ea2e03b3
nikita <nikitazos.of@gmail.com>
2022-11-21 23:59:22 +0300

    update

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => {
-  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   return `Hello, ${userName}!`;
 };
