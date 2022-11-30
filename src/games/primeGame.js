624e6b364a1a048caf63c00c1d63c0c7f4138877
NikitaZosimov <nikitazos.of@gmail.com>
2022-11-30 22:32:00 +0300

    update

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -5,7 +5,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrimeNumber = (num) => {
   for (let i = 2; i < num; i += 1) {
-    if (num % i === 0 && num < 2) {
+    if (num % i === 0 || num < 2) {
       return false;
     }
   }
@@ -14,7 +14,7 @@ const isPrimeNumber = (num) => {
 
 const getQuestionAndAnswer = () => {
   const question = getRandomNumbers(1, 10);
-  const correctAnswer = isPrimeNumber(question) ? 'no' : 'yes';
+  const correctAnswer = isPrimeNumber(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 export default () => gameRules(description, getQuestionAndAnswer);
