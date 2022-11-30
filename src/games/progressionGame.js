6debe4df7ec2bc128165da4ef1b41edc06241a06
NikitaZosimov <nikitazos.of@gmail.com>
2022-11-30 17:50:40 +0300

    update

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -3,17 +3,21 @@ import getRandomNumbers from '../helper.js';
 
 const description = 'What number is missing in the progression?';
 
-const getQuestionAndAnswer = () => {
-  const firstProgressionNumber = getRandomNumbers(1, 100);
+const getProgression = () => {
   const differenceBetweenTheNumbers = getRandomNumbers(1, 10);
-  const indexOfHiddenNumber = getRandomNumbers(0, 9);
-  const progressionNum = [firstProgressionNumber];
+  const progressionNum = [];
   for (let i = 0; i < 9; i += 1) {
     progressionNum[i + 1] = progressionNum[i] + differenceBetweenTheNumbers;
   }
-  const correctAnswer = String(progressionNum[indexOfHiddenNumber]);
-  progressionNum[indexOfHiddenNumber] = '..';
-  const question = progressionNum.join(' ');
+};
+
+const getQuestionAndAnswer = () => {
+  const firstProgressionNumber = getRandomNumbers(1, 100);
+  const indexOfHiddenNumber = getRandomNumbers(0, 9);
+  const progression = getProgression(firstProgressionNumber);
+  const correctAnswer = String(progression[indexOfHiddenNumber]);
+  progression[indexOfHiddenNumber] = '..';
+  const question = progression.join(' ');
   return [question, correctAnswer];
 };
 export default () => gameRules(description, getQuestionAndAnswer);
